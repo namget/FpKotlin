ae955af71d2054b305cd5605e0952f8324a938f0
LeeJaeWoo <yeboykr@naver.com>
2020-06-25 23:08:07 +0900

    chapter3

--- app/src/test/java/com/namget/fpkotlin/Chapter3.kt
+++ app/src/test/java/com/namget/fpkotlin/Chapter3.kt
@@ -1,6 +1,7 @@
 package com.namget.fpkotlin
 
 import org.junit.Test
+import kotlin.math.cos
 import kotlin.math.max
 
 class Chapter3 {
@@ -136,26 +137,147 @@ class Chapter3 {
                     thisIterator.next()
                 else
                     otherIterator.next()
+
             override fun hasNext(): Boolean = thisIterator.hasNext() || otherIterator.hasNext()
         }
     }
 
-    fun repeat(n : Int) : Sequence<Int> = sequenceOf(n) + {repeat(n)}
+    fun repeat(n: Int): Sequence<Int> = sequenceOf(n) + { repeat(n) }
 
-    fun zip(list1 : List<Int>, list2 : List<Int>) : List<Pair<Int,Int>> = when{
+    fun zip(list1: List<Int>, list2: List<Int>): List<Pair<Int, Int>> = when {
         list1.isEmpty() || list2.isEmpty() -> listOf()
         else -> listOf(Pair(list1.head(), list2.head())) + zip(list1.tail(), list2.tail())
     }
-    var memo = Array(100,{-1})
 
-    fun fiboMemoization(n : Int) : Int = when{
+    var memo = Array(100, { -1 })
+
+    fun fiboMemoization(n: Int): Int = when {
         n == 0 -> 0
         n == 1 -> 1
         memo[n] != -1 -> memo[n]
+        else -> {
+            println("fiboMemoization($n")
+            memo[n] = fiboMemoization(n - 2) + fiboMemoization(n - 1)
+            memo[n]
+        }
     }
 
-    fun practice3_10(){
+    fun practice3_10() {
 
+    }
+
+    fun fiboFP(n: Int): Int = fiboFP(n, 0, 1)
+    tailrec fun fiboFP(n: Int, first: Int, second: Int): Int = when (n) {
+        0 -> first
+        1 -> second
+        else -> fiboFP(n - 1, second, first + second)
+    }
+
+    tailrec fun findFixPoint(x: Double = 1.0): Double {
+        return if (x == cos(x)) x else findFixPoint(cos(x))
+    }
+
+    fun findFixPoint(): Double {
+        var x = 1.0
+        while (true) {
+            val y = cos(x)
+            if (x == y) return x
+            x = y
+        }
+    }
+
+    tailrec fun tailrecMaximun(items: List<Int>, acc: Int = Int.MIN_VALUE): Int = when {
+        items.isEmpty() -> error("")
+        items.size == 1 -> {
+            println("")
+            acc
+        }
+        else -> {
+            val head = items.head()
+            val maxVal = if (head > acc) head else acc
+            tailrecMaximun(items.tail(), maxVal)
+        }
+    }
+
+    tailrec fun reverse(str: String, acc: String = ""): String = when {
+        str.isEmpty() -> acc
+        else -> {
+            val reversed = str.head() + acc
+            reverse(str.tail(), reversed)
+        }
+    }
+
+    tailrec fun take(n: Int, list: List<Int>, acc: List<Int> = listOf()): List<Int> = when {
+        0 >= n -> acc
+        list.isEmpty() -> acc
+        else -> {
+            val takeList = acc + listOf(list.head())
+            take(n - 1, takeList)
+        }
+    }
+
+    tailrec fun zip(
+        list1: List<Int>,
+        list2: List<Int>,
+        acc: List<Pair<Int, Int>> = listOf()
+    ): List<Pair<Int, Int>> = when {
+        list1.isEmpty() || list2.isEmpty() -> acc
+        else -> {
+            val zipList = acc + listOf(Pair(list1.head(), list2.head()))
+            zip(list1.tail(), list2.tail(), zipList)
+        }
+    }
+
+//    fun even(n : Int) : Boolean = when(n){
+//        0 -> true
+//        else -> odd(n-1)
+//    }
+//    fun odd(n: Int) : Boolean = when(n) {
+//        0 -> false
+//        else -> even(n-1)
+//    }
+
+    sealed class Bounce<A> {
+        data class Done<A>(val result: A) : Bounce<A>()
+        data class More<A>(val thunk: () -> Bounce<A>) : Bounce<A>()
+    }
+
+    tailrec fun <A> trampoline(bounce : Bounce<A>) : A = when(bounce) {
+        is Bounce.Done-> bounce.result
+        is Bounce.More -> trampoline(bounce.thunk())
+    }
+
+    fun odd(n: Int) : Bounce<Boolean> = when(n) {
+        0 -> Bounce.Done(false)
+        else -> Bounce.More{ even(n-1)}
+    }
+
+    fun even(n : Int) : Bounce<Boolean> = when(n){
+        0 -> Bounce.Done(true)
+        else -> Bounce.More{ odd(n-1)}
+    }
+
+    /*fun<T> powerset(s : Set<T>): Set<Set<T>> = when {
+        s.isEmpty() -> setOf(setOf())
+        else ->{
+            setOf(setOf())
+        }
+    }*/
+
+    fun <T> Set<T>.head() = first()
+    fun <T> Set<T>.tail() = drop(1).toSet()
+    fun<T> powerset(s : Set<T>): Set<Set<T>> = when {
+        s.isEmpty() -> setOf(setOf())
+        else ->{
+            val head = s.head()
+            val restSet = powerset(s.tail())
+            restSet + restSet.map { setOf(head) + it }.toSet()
+        }
+    }
+
+    tailrec fun <T> powerset(s: Set<T>, acc : Set<Set<T>>) : Set<Set<T>> = when{
+        s.isEmpty() -> acc
+        else -> powerset(s.tail(), acc + acc.map{it + s.head()})
     }
 
 
